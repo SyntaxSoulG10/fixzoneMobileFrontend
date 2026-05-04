3373201f24394b9e21365f327083943f659ad274
vichanuka <vichanuka2@gmail.com>
2026-05-04 15:53:00 +0530

    fix: edit UserProvider context and new monochrome Android icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,7 @@ import { useEffect } from "react";
 import { View, ActivityIndicator } from "react-native";
 import CustomDrawer from "../components/navigation/CustomDrawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
+import { UserProvider } from "../context/UserContext";
 import { BookingProvider } from "../context/BookingContext";
 
 function RootLayoutNav() {
@@ -55,7 +55,7 @@ function RootLayoutNav() {
   );
 }
 
-import { UserProvider } from "../context/UserContext";
+
 
 export default function Layout() {
   return (
